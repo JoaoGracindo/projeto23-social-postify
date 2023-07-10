965b6a28124964b5b232fcb72568c3ef8cc062d4
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-07-10 16:21:14 -0300

    Feat: DTOs

--- src/publication/dto/create-publication.dto.ts
+++ src/publication/dto/create-publication.dto.ts
@@ -1,1 +1,29 @@
-export class CreatePublicationDto {}
+import {
+    IsString,
+    IsNotEmpty,
+    IsDateString,
+    IsUrl,
+} from 'class-validator'
+
+export class CreatePublicationDto {
+
+@IsNotEmpty()
+@IsUrl()
+image: string
+
+@IsNotEmpty()
+@IsString()
+title: string
+
+@IsNotEmpty()
+@IsString()
+text
+
+@IsNotEmpty()
+@IsDateString()
+dateToPublish: string
+
+@IsNotEmpty()
+@IsString()
+socialMedia: string
+}
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,31 @@
-export class CreateUserDto {}
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsString,
+    IsStrongPassword,
+    IsUrl
+  } from 'class-validator';
+  
+  export class CreateUserDTO {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+  
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+  
+    @IsString()
+    @IsNotEmpty()
+    @IsStrongPassword({
+      minLength: 6,
+    })
+    password: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsUrl()
+    avatar: string
+  }
+  
