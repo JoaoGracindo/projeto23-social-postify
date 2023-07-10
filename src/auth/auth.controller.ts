8a8f2bbcfcad7834a85b99aa38b88dbae3dfa2ef
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-07-10 17:03:59 -0300

    Feat: auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInDto } from './dto/auth-signin.dto';
+import { SignUpDto } from './dto/auth-signup.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+   @Post('signin')
+   async signin(@Body() body: SignInDto) {
+    return this.authService.signin(body);
+   }
+
+   @Post('signup')
+   async signup(@Body() body: SignUpDto) {
+    return this.authService.signup(body);
+   }
 }
