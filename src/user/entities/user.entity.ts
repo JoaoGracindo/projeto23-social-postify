7b014c2e96ff17a1f6105c5cc39bf2bdfd57e79d
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-07-10 16:49:22 -0300

    Build: jwt

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,14 @@
-export class User {}
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+
+export class User {
+  constructor(
+    private _name: string,
+    private _email: string,
+    private _password: string,
+    private _avatar: string,
+    readonly id: string = randomUUID(),
+  ) {}
+
+  
+}
